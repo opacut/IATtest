079e7dbfa57282a05eb9b12d2505b1ad099b97bb
Oliver Pacut <oliver.pacut@gmail.com>
2016-11-14 21:47:06 +0100

    / in css path

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
 <title>Online IAT</title>	
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<link type="text/css" href="core/css/overcast/jquery-ui-1.8.18.custom.css" rel="stylesheet" />
-<style type="text/css"> @import "core/css/iat.css";</style>	
+<link type="text/css" href="/core/css/overcast/jquery-ui-1.8.18.custom.css" rel="stylesheet" />
+<style type="text/css"> @import "/core/css/iat.css";</style>	
 <script type="text/javascript" src="core/js/jquery-1.7.1.min.js"></script>
 <script type="text/javascript" src="core/js/jquery-ui-1.8.18.custom.min.js"></script>
 <script type="text/javascript" src="core/js/IAT.js"></script>
