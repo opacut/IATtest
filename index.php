87053ef4424358252b3f4d2e6e3d0f82160d538f
Oliver Pacut <oliver.pacut@gmail.com>
2016-11-14 21:56:23 +0100

    css

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 <title>Online IAT</title>	
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link type="text/css" href="/core/css/overcast/jquery-ui-1.8.18.custom.css" rel="stylesheet" />
-<style type="text/css"> @import "/core/css/iat.css";</style>	
+<!--<style type="text/css"> @import "/core/css/iat.css";</style>	-->
+<link type="text/css" href="/core/css/iat.css" rel="stylesheet" />
 <script type="text/javascript" src="core/js/jquery-1.7.1.min.js"></script>
 <script type="text/javascript" src="core/js/jquery-ui-1.8.18.custom.min.js"></script>
 <script type="text/javascript" src="core/js/IAT.js"></script>
